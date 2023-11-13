a1ac617f9caed6741e1ef564b306ac55191abe98
HizkiaSiregar <s22210002@student.unklab.ac.id>
2023-11-13 08:06:56 -0800

    hopefully this work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import ContactInfo from './components/ContactInformation';
+import PersonalInfo from './components/PersonalInformation';
+import EducationInfo from './components/EducationalInformation';
+import SkillsInfo from './components/SkillsAndCompetence';
+import WorkExperience from './components/WorkExperience';
+import AffiliationInfo from './components/Affiliation';
+import ReferencesInfo from './components/References';
+import "./CV.css";
 
-function App() {
+import { Fade } from 'react-reveal';
+
+const App = () => {
+  const fadeDuration = 980;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <Fade duration={fadeDuration}><Header /></Fade>
+        <div className="content">
+          <Fade duration={fadeDuration}><ContactInfo /></Fade>
+          <Fade duration={fadeDuration}><PersonalInfo /></Fade>
+          <Fade duration={fadeDuration}><EducationInfo /></Fade>
+          <Fade duration={fadeDuration}><SkillsInfo /></Fade>
+          <Fade duration={fadeDuration}><WorkExperience /></Fade>
+          <Fade duration={fadeDuration}><AffiliationInfo /></Fade>
+          <Fade duration={fadeDuration}><ReferencesInfo /></Fade>
+        </div>
+      </div>
   );
-}
+};
 
 export default App;
